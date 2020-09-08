d626b61fe7a7942665512965da4825b4da45c863
Ramya Senthamaraikannan <ramyasenthamaraikannan@Ramyas-MacBook-Pro.local>
2020-09-09 00:06:46 +0530

    jwtCheck function without params

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const jwt = require('express-jwt')
 const jwks = require('jwks-rsa');
 
 
-authentication.jwtCheck = (audience = null) => jwt({
+authentication.jwtCheck = jwt({
     secret: jwks.expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 15,
         jwksUri: `https://${process.env.DOMAIN}/.well-known/jwks.json`
     }),
-    //AUDIENCE is the clientId of an application, provide this if you need an API to be validated specifically for a single client
-    audience: audience ? process.env.AUDIENCE : null,
     issuer: `https://${process.env.DOMAIN}/`,
     algorithms: ['RS256']
 });
